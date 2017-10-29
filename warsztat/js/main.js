c524e1b14687af917ec614b6680922a9deed817c
Wiktoria Mazan <wiktoriammazan@gmail.com>
2017-10-29 17:12:45 +0100

    portfolio

--- warsztat/js/main.js
+++ warsztat/js/main.js
@@ -7,12 +7,18 @@ $(document).scroll(function(){
 });
                    
 function addScrolled(){
+    
+    if (window.innerWidth>992) {
     var height = $('#main-nav').height();
     var scroll = $(window).scrollTop();
     
     if(scroll > height) {
         $('#main-nav').addClass('transparent-blue');
+//        $('.navbar-collapse').addClass('transparent-blue2');
     } else {
         $('#main-nav').removeClass('transparent-blue');
+//        $('.navbar-collapse').removeClass('transparent-blue2');
     }
+    }
+
 }
